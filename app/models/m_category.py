be9e65510fbd4d7ece22c7c55f2b2d0163ce9c46
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2025-06-07 01:02:23 +0700

    add model (m_category)

---
+++ app/models/m_category.py
@@ -0,0 +1,11 @@
+from tortoise import fields, models
+
+class MCategory(models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255)
+    slug = fields.CharField(max_length=255)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "m_category"
