5e44d18ecc0ceb94811bc4cc6a167a6a15be3970
Gilang Raka <gilangrakar@gmail.com>
2025-06-15 18:56:39 +0700

    Add __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,31 @@
+from .app_history import AppHistory
+from .blindstick import Blindstick
+from .log_blindstick import LogBlindstick
+from .log_penyandang_cam import LogPenyandangCam
+from .log_penyandang_map import LogPenyandangMap
+from .log_penyandang_status import LogPenyandangStatus
+from .log_user import LogUser
+from .m_category import MCategory
+from .m_tag import MTag
+from .pemantau import Pemantau
+from .penyandang import Penyandang
+from .post import Post
+from .role import Role
+from .user import User
+
+__all__ = [
+    "AppHistory",
+    "Blindstick",
+    "LogBlindstick",
+    "LogPenyandangCam",
+    "LogPenyandangMap",
+    "LogPenyandangStatus",
+    "LogUser",
+    "MCategory",
+    "MTag",
+    "Pemantau",
+    "Penyandang",
+    "Post",
+    "Role",
+    "User"
+]
