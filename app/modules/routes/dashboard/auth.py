96930bd9d663ba928c4172f3362a75dd61d9b1cd
Gilang Raka <gilangrakar@gmail.com>
2025-06-16 17:31:14 +0700

    Fix auth module

--- app/modules/routes/dashboard/auth.py
+++ app/modules/routes/dashboard/auth.py
@@ -4,6 +4,6 @@ from app.modules.controllers.auth import login
 
 router = APIRouter(prefix="/auth", tags=["Auth Dashboard (Superadmin/Admin)"])
 
-router.post("/login", response_model=TokenSchema)
+@router.post("/login", response_model=TokenSchema)
 async def login_handler(token: TokenSchema = Depends(login({"superadmin", "admin"}))):
     return token
--- app/modules/routes/mobile/auth.py
+++ app/modules/routes/mobile/auth.py
@@ -2,20 +2,20 @@ from fastapi import APIRouter, Depends
 from app.modules.schemas import TokenSchema
 from app.modules.controllers.auth import login, register
 
-router = APIRouter(prefix="/auth", tags=["Auth Dashboard (Superadmin/Admin)"])
+router = APIRouter(prefix="/auth", tags=["Auth Mobile (Pemantau/Penyandang)"])
 
-router.post("/login/pemantau", response_model=TokenSchema)
+@router.post("/login/pemantau", response_model=TokenSchema)
 async def login_pemantau_handler(token: TokenSchema = Depends(login({"pemantau"}))):
     return token
 
-router.post("/login/penyandang", response_model=TokenSchema)
+@router.post("/login/penyandang", response_model=TokenSchema)
 async def login_penyandang_handler(token: TokenSchema = Depends(login({"penyandang"}))):
     return token
 
-router.post("/register/pemantau", response_model=TokenSchema)
+@router.post("/register/pemantau", response_model=TokenSchema)
 async def register_pemantau_handler(token: TokenSchema = Depends(register("pemantau"))):
     return token
 
-router.post("/register/penyandang", response_model=TokenSchema)
+@router.post("/register/penyandang", response_model=TokenSchema)
 async def register_penyandang_handler(token: TokenSchema = Depends(register("penyandang"))):
     return token
