89008cbb752f8b891d8ed44dc0ca4740d30519c7
Gilang Raka <gilangrakar@gmail.com>
2025-06-16 16:57:26 +0700

    Add code init and main.py

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from app.core import TORTOISE_ORM
+
+import app.modules.routes.dashboard as dashboard
+import app.modules.routes.mobile as mobile
+
+app = FastAPI()
+
+dashboard_routes = [
+    dashboard.auth_router
+]
+
+mobile_routes = [
+    mobile.auth_router
+]
+
+for route in dashboard_routes:
+    app.include_router(route, prefix="/dashboard")
+
+for route in mobile_routes:
+    app.include_router(route, prefix="/mobile")
+
+register_tortoise(
+    app,
+    config=TORTOISE_ORM,
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
