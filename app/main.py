94bdecc4c4aed8298ac1d258a20e5dc52c8c260e
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2025-07-24 22:11:08 +0700

    Fix

--- app/main.py
+++ app/main.py
@@ -9,14 +9,10 @@ from app.modules.routes.mobile import mobile_router
 
 app = FastAPI()
 
-origins = [
-    "*"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=False,
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
