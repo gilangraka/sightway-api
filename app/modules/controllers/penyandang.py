931c37a1de5aff66b9e80bd25471dd2fbf9a1d46
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2025-08-01 04:35:14 +0700

    Add send emergency

--- app/modules/controllers/penyandang.py
+++ app/modules/controllers/penyandang.py
@@ -3,6 +3,7 @@ from typing import Optional
 from app.models import Pemantau, Penyandang, PenyandangPemantau
 from app.core.firebase import db
 from datetime import datetime
+from app.helpers.fcm_service import FCMService
 
 async def accept_invitation(
     pemantau_id: int,
@@ -57,6 +58,56 @@ async def list_pemantau(penyandang_id: int, status_filter: Optional[str] = None)
         return {
             "data": filtered
         }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+    
+async def send_emergency_to_pemantau(penyandang_id: int):
+    fcm = FCMService("firebase_credential.json", "sightway-9f360")
+    try:
+        query = PenyandangPemantau.filter(penyandang_id=penyandang_id)
+
+        queryset = await query.select_related("pemantau", "pemantau__user", "penyandang", "penyandang_user").values(
+            'pemantau__user__id',
+            'penyandang__user__name'
+        )
+
+        for record in queryset:
+            user_id = record["pemantau__user__id"]
+            nama_penyandang = record["penyandang__user__name"]
+            ref = db.reference(f"/pemantau/{user_id}")
+
+            data = ref.get()
+            
+            if data is None:
+                continue
+
+            token = data.get("fcm_token")
+            if token:
+                status_code, result = fcm.send_fcm_message(
+                    target_token=token,
+                    title="Penyandang terdeteksi darurat!",
+                    body=(
+                        f"Gawattt! Penyandang yang kamu pantau dengan nama {nama_penyandang} "
+                        "terdeteksi darurat!!! Cepat cek dan monitoring sekarang!"
+                    ),
+                    data={"click_action": "FLUTTER_NOTIFICATION_CLICK"}
+                )
+
+                if status_code == 200:
+                    saved = fcm.save_notification_to_database(
+                        user_id=data.user_id,
+                        title=data.title,
+                        body=data.body
+                    )
+                    if not saved:
+                        raise HTTPException(status_code=500, detail="Notification sent but failed to save to database.")
+                    return {"message": "Notification sent and saved successfully"}
+                else:
+                    raise HTTPException(status_code=500, detail=result)
 
     except Exception as e:
         raise HTTPException(
--- app/modules/routes/mobile/penyandang.py
+++ app/modules/routes/mobile/penyandang.py
@@ -3,7 +3,7 @@ from typing import Optional
 from fastapi import APIRouter, Depends, Query
 from app.middleware import auth_middleware, role_middleware
 from app.models import Penyandang
-from app.modules.controllers.penyandang import accept_invitation, list_pemantau
+from app.modules.controllers.penyandang import accept_invitation, list_pemantau, send_emergency_to_pemantau
 from app.modules.schemas.add_invitation import AcceptInvitationSchema
 
 router = APIRouter(prefix="/penyandang", tags=["API Mobile Penyandang"])
@@ -22,6 +22,14 @@ async def add_invitation_penyandang_handler(
         detail_status_pemantau=request.detail_status_pemantau,
         penyandang_id=penyandang.id,
     )
+
+@router.get("/send-emergency-to-pemantau")
+async def send_emergency_handler(
+    user_id: int = Depends(auth_middleware),
+    _ = Depends(role_middleware(["penyandang"]))
+):
+    penyandang = await Penyandang.get_or_none(user_id=user_id)
+    return await send_emergency_to_pemantau(penyandang_id=penyandang.id)
 
 @router.get("/list-pemantau")
 async def list_pemantau_handler(   
